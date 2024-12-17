ca8f37ec1f0287e4a00d8685f37863f15552bba8
HenryKirykovich <145735504+HenryKirykovich@users.noreply.github.com>
2024-12-17 14:05:43 -0800

    Merge branch 'master' of https://github.com/HenryKirykovich/CSharpSmallest

---
+++ SimpleObject_do_foreach/fruit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleObject_do_foreach
+{
+    internal class Fruit
+    {
+        private string _FruitName;
+        private string _Color;
+
+
+        public Fruit() { } // default 
+
+        public Fruit(string fruit, string color)
+        {
+            _FruitName = fruit;
+            _Color = color;
+        }
+
+
+       
+
+
+        public static void Print(Fruit[] fruits)
+        {
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. {fruits[i]._FruitName} has color {fruits[i]._Color}");
+            }
+        }
+
+    }
+}

dd6dba4da57450f48c1089a9679233baa2df20fe
HenryKirykovich <145735504+HenryKirykovich@users.noreply.github.com>
2025-01-13 16:03:24 -0800

    fix namecpace

--- Lambda/Lambda_4/Program.cs
+++ Lambda/Lambda_4/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Lambda4
+namespace Lambda_4
 {
     internal class Program
     {
--- Lambda/Lambda_9/Program.cs
+++ Lambda/Lambda_9/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lambda9
+namespace Lambda_9
 {
     internal class Program
     {

ae09c9fa8a03964e6e33d1f1492a5c8c36af4839
HenryKirykovich <145735504+HenryKirykovich@users.noreply.github.com>
2025-01-13 16:01:14 -0800

    added more

--- Lambda/Lambda9/Program.cs
+++ Lambda/Lambda9/Program.cs
@@ -40,12 +40,19 @@ namespace Lambda9
             var new_list = list.Where(n => n > 80); // create a list of numbers and display numbers greater than 80
             Console.WriteLine();
             Console.WriteLine("Result:");
+            
+            if(!new_list.Any())
+            {
+                Console.WriteLine("the list is empty");
+            }
+
             foreach (int i in new_list)
             {
                 count += i;
                 Console.WriteLine($"The {count} number greater than 80 {i}");
 
             }
+
         }
     }
 }
